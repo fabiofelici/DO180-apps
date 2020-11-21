4c6ca8e4110c6c8733e3bd71a483bb6c5e20782c
Fabio Felici <ffelici@extraordy.com>
2020-11-21 12:01:08 +0100

    65428903

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,6 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "<br />"
-print "<img src=https://www.redhat.com/cms/managed-files/Logo-Red_Hat-OpenShift_Container_Platform-B-Standard-RGB.png>"
+print "<img src=https://www.redhat.com/cms/managed-files/Logo-Red_Hat-OpenShift_Container_Platform-B-Standard-RGB.pngi />"
 print "Sto cambiando roba"
 ?>
