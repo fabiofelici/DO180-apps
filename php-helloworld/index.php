1991efd8955ba526b4449d8f253552a0fe244af4
Fabio Felici <ffelici@extraordy.com>
2020-11-21 17:17:04 +0100

    65428907

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,5 +2,4 @@
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "<br />";
 print "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/OpenShift-LogoType.svg/200px-OpenShift-LogoType.svg.png' />";
-print "Sto cambiando roba";
 ?>
